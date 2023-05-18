d2c4cf380122d1d22cb6dcac7efdd68c53516155
Vendann <tigrun324@gmail.com>
2023-05-18 16:17:15 +0300

    1

---
+++ Assets/Scripts/CameraMover.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+	public Transform player;
+	private Vector3 pos;
+
+	void Start()
+	{
+		player = GameObject.Find("Player").transform;
+	}
+
+	void LateUpdate()
+	{
+		pos = player.position;
+		pos.y = 13f;
+		this.transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+	private Rigidbody rb;
+	private float hInput;
+	private float vInput;
+	public float speed = 4f;
+	public float rotationSpeed = 10f;
+	private Vector3 directionVector;
+	
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+		animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+		vInput = Input.GetAxis("Vertical");
+		directionVector = new Vector3(hInput, 0, vInput);
+		animator.SetFloat("speed", Vector3.ClampMagnitude(directionVector, 1).magnitude);
+		if (directionVector.magnitude > Mathf.Abs(0.05f))
+			transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(directionVector), Time.deltaTime * rotationSpeed);
+		if (Input.GetMouseButton(0))
+		{
+			int i = Random.Range(1, 7);
+			animator.Play(i.ToString());
+		}
+    }
+	
+	void FixedUpdate()
+	{
+		rb.velocity = Vector3.ClampMagnitude(directionVector, 1) * speed;
+		rb.angularVelocity = Vector3.zero;
+	}
+}
